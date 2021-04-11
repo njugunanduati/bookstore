9bb3b1be0f74f8e2dfcd71f78878eff00efa6966
James Njuguna <james@arifu.com>
2021-04-11 20:14:33 +0300

    --updated the config SQLALCHEMY_DATABASE_URI

--- app/config.py
+++ app/config.py
@@ -8,7 +8,8 @@ class Config(object):
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
     DB_NAME = os.environ.get('DB_NAME')
     DB_HOST = os.environ.get('DB_HOST')
-    SQLALCHEMY_DATABASE_URI = f'{DB_ENGINE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+    # SQLALCHEMY_DATABASE_URI = f'{DB_ENGINE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
